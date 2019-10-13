72793e62f578ce803a0dfe59fee2f88ba3e76363
Benjamin Broestl <Bensgamingmail@yahoo.com>
2019-10-13 16:02:31 -0400

    Implemented selected video and styling. Updated Readme

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,14 @@ import React from "react";
 import SearchBar from "./SearchBar";
 import youtube from "../apis/youtube";
 import VideoList from "./VideoList";
+import VideoDetail from "./VideoDetail";
 
 class App extends React.Component {
-  state = { videos: [] };
+  state = { videos: [], selectedVideo: null };
+
+  componentDidMount() {
+    this.onTermSubmit("10 simple ttricks search youtube");
+  }
 
   onTermSubmit = async term => {
     const response = await youtube.get("/search", {
@@ -14,7 +19,14 @@ class App extends React.Component {
       }
     });
     this.setState({
-      videos: response.data.items
+      videos: response.data.items,
+      selectedVideo: response.data.items[0]
+    });
+  };
+
+  onVideoSelect = video => {
+    this.setState({
+      selectedVideo: video
     });
   };
 
@@ -22,7 +34,19 @@ class App extends React.Component {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit} />
-        <VideoList videos={this.state.videos} />
+        <div className="ui grid">
+          <div className="ui row">
+            <div className="eleven wide column">
+              <VideoDetail video={this.state.selectedVideo} />
+            </div>
+            <div className="five wide column">
+              <VideoList
+                videos={this.state.videos}
+                onVideoSelect={this.onVideoSelect}
+              />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -2,11 +2,15 @@ import React from "react";
 
 import VideoItem from "./VideoItem";
 
-const VideoList = ({ videos }) => {
+const VideoList = ({ videos, onVideoSelect }) => {
   const renderedList = videos.map(video => {
     return (
-      <div className="ui relaxed divided list">
-        <VideoItem video={video} />
+      <div className="ui relaxed divided list" key={video.id.videoId}>
+        <VideoItem
+          video={video}
+          onVideoSelect={onVideoSelect}
+          key={video.id.videoId}
+        />
       </div>
     );
   });
